156da1f11b0b573693196ff5c4a78b24604dcdd8
Rajib Hossain <rajibhossain8521@gmail.com>
2020-06-16 00:14:40 +0600

    url mappring for searching by writer_name and trees_name

--- trees_info/urls.py
+++ trees_info/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('info/all/', views.index_info, name='index_info'),
+    #path('search/by/trees/<str:tree_name>', views.search_by_trees_name, name='search_by_trees_name'),
+    #path('search/by/writer/<str:writername>', views.search_by_user_id, name='search_by_writer_name'),
 ]
