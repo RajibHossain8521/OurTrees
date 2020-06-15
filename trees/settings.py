fe2784fd480a590cc08babb2b1eae66308ae8872
Rajib Hossain <rajibhossain8521@gmail.com>
2020-06-15 20:14:20 +0600

    Add rest_framework library and trees_info app

--- trees/settings.py
+++ trees/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party libraray or package
+    'rest_framework',
+
+    # Developers app
+    'trees_info',
 ]
 
 MIDDLEWARE = [
